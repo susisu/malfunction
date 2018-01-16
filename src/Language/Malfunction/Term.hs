4d6fdbb87fd76c45b42f0dcc4e7884223a26413f
Susisu <susisu2413@yahoo.co.jp>
2018-01-16 19:54:07 +0900

    Add Term data

--- src/Language/Malfunction/Term.hs
+++ src/Language/Malfunction/Term.hs
@@ -13,3 +13,20 @@ data Pattern = PatVar Id           -- variable
              | PatLit Literal      -- literal
              | PatAs Id Pattern    -- as-pattern
              | PatCon Id [Pattern] -- constructor
+
+data Term a = TmVar a Id                        -- variable
+            | TmLit a Literal                   -- literal
+            | TmApp a (Term a) [Term a]         -- application
+            | TmAbs a Pattern (Term a)          -- lambda-abstraction
+            | TmLet a Pattern (Term a) (Term a) -- let-in binding
+            | TmIf a (Term a) (Term a) (Term a) -- if-then-else
+            | TmCase a [(Pattern, Term a)]      -- pattern matching
+
+getInfo :: Term a -> a
+getInfo (TmVar i _)     = i
+getInfo (TmLit i _)     = i
+getInfo (TmApp i _ _)   = i
+getInfo (TmAbs i _ _)   = i
+getInfo (TmLet i _ _ _) = i
+getInfo (TmIf i _ _ _)  = i
+getInfo (TmCase i _)    = i
